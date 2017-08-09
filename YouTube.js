b921f8681cb6d74fdc685d38e7ce053df5c3ae74
tylerjusto <tbjusto2014@gmail.com>
2017-08-08 23:54:20 -0400

    Create YouTube.js

---
+++ YouTube.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import 'bulma/css/bulma.css';
+
+export default function YouTube(props){
+
+    return(
+        <div style={{position:'relative', height:0, paddingBottom: '56.25%'}}>
+            <iframe
+                src="https://www.youtube.com/embed/K8Dkr2r--ig"
+                width={640} height={360} frameBoarder={0}
+                style={{
+                    position: 'absolute',
+                    width: '30%',
+                    height: '30%',
+                    left:0
+                }}
+                allowFullScreen
+            />
+        </div>
+
+    );
+
+}
